2a9bdcad23184c0ee4f71a8717174f76161d72b0
Davi Sandro <davisandro300@gmail.com>
2022-08-19 22:04:38 -0300

    Terminei

---
+++ components/Form.js
@@ -0,0 +1,52 @@
+import react, {Component} from "react";
+import { Button, TextInput, View } from "react-native";
+
+class Form extends Component{
+
+    static defaultProps = {
+
+        onAdd: () =>{
+
+        }
+    }
+
+    state = {
+
+        text: ''
+    }
+
+    add = () =>{
+        const {state} = this;
+        if(state.text){
+
+            this.props.onAdd(state.text)
+            this.handleChange('')
+
+        }
+
+    }
+
+    handleChange =(text) =>{
+
+        this.setState({text})
+    }
+
+    render(){
+
+        const {state} = this;
+        return(
+
+            <View>
+
+                <TextInput value={state.text} onChangeText={this.handleChange}/>
+                <Button title="Adicionar" color="#03045e" onPress={this.add}/>
+
+
+            </View>
+
+        )
+    }
+
+}
+
+export default  Form;
